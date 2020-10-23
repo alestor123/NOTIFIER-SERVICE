84819f4528eae39fcec978e5eb83b8d9fcf84f96
alestor123 <alestoraldous@gmail.com>
2020-10-23 15:40:31 +0530

    added notifiere code

--- App.js
+++ App.js
@@ -8,7 +8,10 @@ app.get('/', (req, res) => {
     res.json({'info':'This is the server'})
 })
 app.post('/api/v1', (req, res) => {
-    
+    notifier.notify({
+        title: req.body.title,
+        message: req.body.message
+      });  
 })
 app.listen(port, () => console.log(`server running at ${port}`))
 
