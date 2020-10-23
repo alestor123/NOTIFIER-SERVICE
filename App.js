7036dd83e8d31ee0a4664bcd9c8768f22861c304
alestor123 <alestoraldous@gmail.com>
2020-10-23 15:33:57 +0530

    added port

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
 var express = require('express'),
     app = express(),
-    notifier = require('node-notifier');
+    notifier = require('node-notifier'),
+port = process.env.PORT || 3000;
